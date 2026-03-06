65ea34f1c88a01d4efe2066b8bcfb90fe693f3d0
deekshitha7405-lgtm <deekshitha7405@gmail.com>
2026-03-06 14:47:44 +0530

    chatbot

--- ai_teacher/ai_teacher/settings.py
+++ ai_teacher/ai_teacher/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-78tr&secqq8td&t)1&k)ern$xl-h+jf901+)#3p#!e)$6nd+jn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
